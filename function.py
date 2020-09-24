d9e3e835d810b11aa89fffe96eaf7b5fe3e055a0
aggarwaltanmay <69584477+aggarwaltanmay@users.noreply.github.com>
2020-09-24 21:40:35 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,17 @@
+def function(pop):
+    genre = pop
+    return genre
+Var = "pop"
+print(Var)
+
+def function(maroon5):
+    artist = maroon5
+    return artist
+Var = "maroon 5"
+print(Var)
+
+def function(memories):
+    song = memories
+    return song
+Var = "memories"
+print(Var)
