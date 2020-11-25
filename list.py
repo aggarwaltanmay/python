ebb2e47eaff92c26aabce1bca59cd6255218f360
aggarwaltanmay <69584477+aggarwaltanmay@users.noreply.github.com>
2020-11-25 09:54:48 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+myUniqueList = []
+myUniqueList.append("Harvey")
+print(myUniqueList)
+myUniqueList.append("Mike")
+print(myUniqueList)
+myUniqueList.append("Jessica")
+print(myUniqueList)
+myUniqueList.insert(2,'Louis')
+print(myUniqueList)
+myUniqueList.insert(4,'Donna')
+print(myUniqueList)
+myUniqueList.insert(3,'Rachel')
+print(myUniqueList)
+
+if(myUniqueList):
+    print(True)
+else:
+    print(False)
